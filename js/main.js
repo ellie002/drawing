8c69a35ad3ba64904f493a0cae58e861f6d3a1c8
ellie002 <elis.kanova@seznam.cz>
2022-01-20 18:41:22 +0100

    02 - Buttons

--- js/main.js
+++ js/main.js
@@ -3,12 +3,16 @@ canvas.width = window.innerWidth - 60;
 canvas.height = 400;
 
 let context = canvas.getContext("2d");
-context.fillStyle = "white";
+let start_background_color = "white";
+context.fillStyle = start_background_color;
 context.fillRect(0, 0, canvas.width, canvas.height);
 
 let draw_color = "black";
 let draw_width = "2";
 let is_drawing = false;
+
+let restore_array = [];
+let index = -1;
 
 function change_color(element) {
     draw_color = element.style.background;
@@ -50,5 +54,31 @@ function stop(event) {
         context.closePath();
         is_drawing = false;
     }
-    event.preventDefault();          
+    event.preventDefault();     
+
+    if ( event.type != 'mouseout' ) {
+        restore_array.push(context.getImageData(0, 0, canvas.width, canvas.height));
+        index += 1;
+    }
+    console.log(restore_array);
+}
+
+function clear_canvas() {
+    context.fillStyle = start_background_color;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    restore_array = [];
+    index = -1;
+}
+
+function undo_last() {
+    if ( index <= 0) {
+        clear_canvas();
+    } else {
+        index -= 1;
+        restore_array.pop();
+        context.putImageData(restore_array[index], 0, 0);
+    }
+
 }
